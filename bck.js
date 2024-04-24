de2e354fd31191ccb0d93fbd932c3d0ec48768f9
Dávio Carvalho <129198119+Davio27@users.noreply.github.com>
2024-04-23 21:43:01 -0300

    Update bck.js

--- bck.js
+++ bck.js
@@ -37,7 +37,7 @@ window.onscroll = () => {
 
 
 var typed = new Typed(".multiple-texto", {
-    strings: ["Desenvolvedor Web", "Desenvolvedor FullStack Node.JS", "Desenvolvedor React.JS"],
+    strings: ["Desenvolvedor Web", "Desenvolvedor FullStack", "Desenvolvedor React.JS", "Desenvolvedor React Native"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
